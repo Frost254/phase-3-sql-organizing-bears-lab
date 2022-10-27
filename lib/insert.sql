4574f635b6c149b604ee7e4e293ee51e10fd36c3
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-27 22:22:05 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "black", "angry", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "black", "angry", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2, "F", "black", "loving", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "black", "angry", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "black", "angry", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 11, "M", "black", "very angry", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 22, "F", "black", "happy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 4, "M", "black", "angry", 1);
